dfb475c301963ca7768e328ac2641c59003e1f11
David <sipos.david.daniel@gmail.com>
2020-10-01 18:08:58 +0200

    changed ngx-boostrap to ng-bootstrap

--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module.ts
@@ -1,15 +1,14 @@
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
+// import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+// import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { CommonModule } from '@angular/common';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { NativeElementInjectorDirective } from './directives/native-element-injector.directive';
 import { NgxIntlTelInputComponent } from './ngx-intl-tel-input.component';
 
-export const dropdownModuleForRoot: ModuleWithProviders<BsDropdownModule> = BsDropdownModule.forRoot();
-export const tooltipModuleForRoot: ModuleWithProviders<TooltipModule> = TooltipModule.forRoot();
 
 @NgModule({
 	declarations: [NgxIntlTelInputComponent, NativeElementInjectorDirective],
@@ -17,8 +16,7 @@ export const tooltipModuleForRoot: ModuleWithProviders<TooltipModule> = TooltipM
 		CommonModule,
 		FormsModule,
 		ReactiveFormsModule,
-		dropdownModuleForRoot,
-		tooltipModuleForRoot,
+		NgbDropdownModule
   ],
 	exports: [NgxIntlTelInputComponent, NativeElementInjectorDirective],
 })
